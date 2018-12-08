ee275f8130a8372ccb8b9f928bfbdf6d4ea207fd
轻剑快马 <xiaoranran1993@outlook.com>
2018-12-08 23:53:35 +0800

    fact: add deploy.js

---
+++ deploy.js
@@ -0,0 +1,23 @@
+const http = require('http')
+const shell = require('shelljs')
+const port = 12315
+
+const server = http.createServer((req, res) => {
+	if (req.url === '/deploy') {
+		if (!shell.which('git')) {
+			shell.echo('Sorry, this script requires git')
+			shell.exit(1)
+		} else {
+			shell.echo('----- Deploy Start -----\n')
+			shell.exec('git pull')
+			shell.exec('pm2 restart hook-test')
+			shell.echo('----- Deploy End -----\n')
+			shell.exit(0)
+		}
+	}
+	res.end('hello')
+})
+
+server.listen(port, () => {
+	console.log(`Server listening on port: ${port}`)
+})
